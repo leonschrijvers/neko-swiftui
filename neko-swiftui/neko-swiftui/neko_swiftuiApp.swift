8edd624951745c41f666de78d92c349e3bd604ec
Leon Schrijvers <leon@isgaar.nl>
2024-12-31 09:52:39 +0100

    Project setup

---
+++ neko-swiftui/neko-swiftui/neko_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  neko_swiftuiApp.swift
+//  neko-swiftui
+//
+//  Created by Leon Schrijvers on 31/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct neko_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
